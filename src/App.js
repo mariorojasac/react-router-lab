44e3a8e7d0802ec8d5999e728fe2c69463585c42
mariorojasac <77466868+mariorojasac@users.noreply.github.com>
2021-10-21 16:20:02 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import {Route, Switch} from 'react-router-dom'
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <Route></Route>
+      <Route></Route>
+      <Route></Route>
+      <Route></Route>
+    </div>
+  );
+}
+
+export default App;
